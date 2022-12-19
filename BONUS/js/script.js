9739d5c664005793815be2b0edbbeb835067573f
robertvlad <vladrob96@gmail.com>
2022-12-19 05:18:53 +0100

    bonus 1 and 2

--- BONUS/js/script.js
+++ BONUS/js/script.js
@@ -1,3 +1,5 @@
+//BONUS 1 E 2
+
 //Creare un'array di oggetti
 
 const imagesArray = [
@@ -24,14 +26,49 @@ const imagesArray = [
     {
         image: "05.webp",
         title: "Marvel Avengers",
-        description: "Marvel's The Avengers or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team",
+        description: "Marvel's The Avengers or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the s",
     },
 ]
+
+// DEFINZIONE FUNZIONE PER AUTOPLAY
+
+function autoplaySlide(){
+
+    if(itemActive < items.length - 1){
+
+        // Togliere la classe active all'imagine e del pallino attuale
+        items[itemActive].classList.remove("active");
+        circles[itemActive].classList.remove("active");
+        thumbnails[itemActive].classList.remove("active")
+
+        // Incremento
+        itemActive++;
+
+        // Aggiungere la classe active all'elemento successivo
+
+        items[itemActive].classList.add("active")
+        circles[itemActive].classList.add("active")
+        thumbnails[itemActive].classList.add("active")
+
+    }else{
+        items[itemActive].classList.remove("active");
+        circles[itemActive].classList.remove("active");
+        thumbnails[itemActive].classList.remove("active")
+
+        itemActive = 0;
+
+        items[itemActive].classList.add("active")
+        circles[itemActive].classList.add("active") 
+        thumbnails[itemActive].classList.add("active")
+    }
+
+}
 
 
 //Creare dinamicamente i div con le immagini del carosello
 
 let itemsContent = '';
+let itemsThumbnails = ""
 
 for(let i = 0; i < imagesArray.length; i++){
     itemsContent += 
@@ -44,6 +81,14 @@ for(let i = 0; i < imagesArray.length; i++){
         </div>
     </div>
     `
+
+    itemsThumbnails += 
+    `
+    <div class="thumb">
+        <img src="./img/${imagesArray[i].image}">
+    </div>
+    `
+
 }
 
 
@@ -51,6 +96,9 @@ for(let i = 0; i < imagesArray.length; i++){
 
 const itemsSlider = document.querySelector('.item-slider');
 itemsSlider.innerHTML += itemsContent;
+
+const thumbnailsPrev = document.querySelector(".thumbnails")
+thumbnailsPrev.innerHTML += itemsThumbnails
 
 
 //Prendere la prima immagine dell'array e renderla attiva
@@ -67,58 +115,43 @@ const circles = document.getElementsByClassName('circle');
 
 circles[itemActive].classList.add('active');
 
+const thumbnails = document.getElementsByClassName("thumb")
+thumbnails[itemActive].classList.add("active")
+
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 
-next.addEventListener('click', function(){
-
-    if(itemActive < items.length - 1){
-
-        // Togliere la classe active all'imagine e del pallino attuale
-        items[itemActive].classList.remove("active");
-        circles[itemActive].classList.remove("active");
-
-        // Incremento
-        itemActive++;
-
-        // Aggiungere la classe active all'elemento successivo
-
-        items[itemActive].classList.add("active")
-        circles[itemActive].classList.add("active")
-
-    }else{
-        items[itemActive].classList.remove("active");
-        circles[itemActive].classList.remove("active");
-
-        itemActive = 0;
-
-        items[itemActive].classList.add("active")
-        circles[itemActive].classList.add("active") 
-    }
-});
+next.addEventListener('click', autoplaySlide)
 
 prev.addEventListener('click', function(){
-
+    
     if(itemActive > 0){
 
         // Togliere la classe active all'imagine e del pallino attuale
         items[itemActive].classList.remove("active");
         circles[itemActive].classList.remove("active");
+        thumbnails[itemActive].classList.remove("active")
 
         // Decremento
         itemActive--;
 
         // Aggiungere la classe active all'elemento precedente
+
         items[itemActive].classList.add("active")
         circles[itemActive].classList.add("active")
+        thumbnails[itemActive].classList.add("active")
 
     }else{
         items[itemActive].classList.remove("active");
         circles[itemActive].classList.remove("active");
+        thumbnails[itemActive].classList.remove("active")
 
         itemActive = items.length - 1;
 
         items[itemActive].classList.add("active")
         circles[itemActive].classList.add("active") 
+        thumbnails[itemActive].classList.add("active")
     }
 })
+
+setInterval(autoplaySlide, 3000);
